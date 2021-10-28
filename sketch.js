12f6c2700da97d479f6b166971e01ac40d75da91
Himani Kumawat <himanikumawat303@gmail.com>
2021-10-28 21:31:22 +0530

    Stage 2 - timer

--- sketch.js
+++ sketch.js
@@ -6,13 +6,19 @@ var bg = 20;
 var line_x1, line_y1, line_lengthx, line_lengthy = 0;
 var r, g, b;
 var info = false;
-let button, radio;
+let button, radio, reaction;
 var font_size;
 var infoButton_size, ib_radius, ib_xpos, ib_ypos, d;
 var oct_size, content_size;
 var min_csize, max_csize;
 var oct_posx, oct_posy;
-var button_posy, button_posx;
+var button_posy, button_posx, stroke_arc, arc_size;
+var timer_buttonx, timer_buttony, timer_button,count1, count, seconds, timefont_size, timer_radius;
+var timeline = 1;
+var timer_start = false;
+var timervalue = 0;
+var seconds_time = 60;
+var color_start = '#ffffff';
 
 //responsive size
 var size_w = screen.width;
@@ -49,6 +55,7 @@ function setup() {
   rectMode(CENTER);
   imageMode(CENTER);
   ellipseMode(CENTER);
+  // frameRate(60);
 
   //Responsive position
   if(size_w > 1200){//desktop
@@ -59,12 +66,21 @@ function setup() {
         oct_posx = windowWidth-windowWidth;
         oct_posy = windowHeight-windowHeight-60;
         button_posy = windowHeight-120;
-        button_posx = windowWidth/2-170;
+        button_posx = windowWidth/2-220;
         font_size = '24px';
         infoButton_size = 50;
-        ib_radius = '25px'
-        ib_xpos = windowWidth - 400;
+        ib_radius = '25px';
+        ib_xpos =  250;
         ib_ypos = windowHeight - windowHeight+80;
+        timer_buttonx = windowWidth - 350;
+        timer_buttony = windowHeight - windowHeight+80;
+        timefont_size = '22px';
+        timer_radius = '50px';
+        timer_buttonsize = infoButton_size+20;
+        arc_posx=timer_buttonx+35;
+        arc_posy= timer_buttony+35;
+        stroke_arc = 3;
+        arc_size = timer_buttonsize+20;
     }
   }
   else if(size_w < 1200){
@@ -73,14 +89,23 @@ function setup() {
     }
     else{//phones
       oct_posx = windowWidth-windowWidth;
-      oct_posy = windowHeight-windowHeight-170;
+      oct_posy = windowHeight-windowHeight-100;
       button_posy = windowHeight-300;
-      button_posx = windowWidth/2-295;
+      button_posx = windowWidth/2-400;
       font_size = '50px';
       infoButton_size = 60;
       ib_radius = '40px';
-      ib_xpos = windowWidth - 200;
-      ib_ypos = windowHeight - windowHeight+100;
+      ib_xpos = 150;
+      ib_ypos = windowHeight - windowHeight+150;
+      timer_buttonx = windowWidth - 300;
+      timer_buttony = windowHeight - windowHeight+120;
+      timefont_size = '45px';
+      timer_radius = '60px';
+      timer_buttonsize = infoButton_size+60;
+      arc_posx=timer_buttonx+60;
+      arc_posy= timer_buttony+60;
+      stroke_arc = 5;
+      arc_size = timer_buttonsize+40;
     }
   }
 
@@ -103,6 +128,7 @@ function setup() {
   radio.option(2, 'Like');
   radio.option(3, 'Comment');
   radio.option(4, 'Share');
+  radio.option(5, 'Report');
   radio.style('font-size', font_size)
   radio.style('checked','2');
   radio.style('color','#ffffff')
@@ -115,6 +141,7 @@ function setup() {
   g = 50;
   b = 50;
   strokeWeight(2);
+  angleMode(DEGREES)
 }
 
 
@@ -133,19 +160,87 @@ function draw() {
   button.style('background-color', col);
   button.style('border-radius', ib_radius);
   button.size(infoButton_size,infoButton_size);
-  button.touchStarted(infoButton);
-  button.mousePressed(infoButton);
+
+  timer_button = createButton('Start')
+  timer_button.position(timer_buttonx,timer_buttony)
+  timer_button.size(timer_buttonsize,timer_buttonsize);
+  timer_button.style('color','#222222')
+  timer_button.style('font-size', timefont_size);
+  timer_button.style('border-color', '#ffffff');
+  timer_button.style('border-radius', timer_radius);
+  timer_button.style('background-color', color_start);
+  // timer_button.touchStarted(timer);
+  timer_button.mousePressed(timerCountdown);
+  // console.log(timer_start);
+
+
+
+
+  if(radio.value() == 1){
+    if(info == false){
+      r = 50;
+      g = 50;
+      b = 50;
+    }
+      // button.touchStarted(infoButton);  //not working properly
+      button.mousePressed(infoButton);
+  }
+  else{
+    r = 120;
+    b = 120;
+    g = 120;
+    info = false;
+  }
 
 //Record the reaction and animation
-  var reaction = radio.value();
+if(radio.value() == 1){
+  if(info == true){
+    reaction = "More information";
+  }
+  else{
+    reaction = "Ignore";
+  }
+}
+else{
+  if(radio.value()==2){
+    reaction = "Like";
+  }
+  else if(radio.value()==3){
+    reaction = "Comment";
+  }
+  else if(radio.value()==4){
+    reaction = "Share";
+  }
+  else if(radio.value()==5){
+    reaction = "Report";
+  }
+}
   // console.log(reaction);
-  // if(reaction == 2){
-  //   // console.log('Like');
-  // }
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+//timer
+function timerCountdown() {
+  color_start = '#222222';
+  timer_start = true;
+  setInterval(function() {
+    if (timervalue < seconds_time) {
+      timervalue++;
+      console.log(timervalue);
+      seconds = map(timervalue,0,seconds_time,0,360);
+      push();
+      stroke('white')
+      strokeWeight(stroke_arc)
+      noFill();
+      arc(arc_posx,arc_posy,arc_size,arc_size,0,seconds);
+      pop();
+    }else{
+      timer_start = false;
+    }
+  }, 1000);
 }
 
 //Information button event
